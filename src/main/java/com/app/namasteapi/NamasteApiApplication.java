de8911c56168957fe19ea5c79c19ed753d985ca8
Rahul Rajagopal <rahulrajagopal38@gmail.com>
2025-09-03 23:34:54 +0530

    Spring and Dependencies Initialized

---
+++ src/main/java/com/app/namasteapi/NamasteApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.namasteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamasteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamasteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/namasteapi/NamasteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.namasteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamasteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
